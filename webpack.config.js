0333f8da2f4c2ef144a13091489104a2540e85ee
WangHan <464023790@qq.com>
2019-03-06 11:57:42 +0800

    '1.0.2'

--- webpack.config.js
+++ webpack.config.js
@@ -100,7 +100,7 @@ if (process.env.NODE_ENV === 'production') {
       }
     }),
     new webpack.optimize.UglifyJsPlugin({
-      sourceMap: false,
+      sourceMap: true,
       compress: {
         warnings: false
       }
