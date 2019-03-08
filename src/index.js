669be5d7f76d5df346e559df6c1b98c067f18d92
WangHan <464023790@qq.com>
2019-03-08 16:52:22 +0800

    'update'

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // sumFunction 插件对应组件的名字
-import chartTcsl from './chart';
+import chartTcsl from './myPlugin/echarts-template/chart.vue';
 
 // Vue.js 的插件应当有一个公开方法 install 。第一个参数是 Vue 构造器，第二个参数是一个可选的选项对象
 // 参考：https://cn.vuejs.org/v2/guide/plugins.html#%E5%BC%80%E5%8F%91%E6%8F%92%E4%BB%B6
